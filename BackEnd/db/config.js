0b60077109b9393ecf203eec5d3b412f79d7de9f
Rithesh D <ritheshdavid@gmail.com>
2024-03-27 15:36:44 +0530

    Update config.js

--- BackEnd/db/config.js
+++ BackEnd/db/config.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 // const uri = "mongodb://localhost:27017/prisoner";
 const uri = process.env.DATABASE_URL;
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-).then(() => {
+mongoose.connect(uri)
+.then(() => {
     console.log("DB CONNECTED")
 }).catch((err) => {
     console.log(err)
